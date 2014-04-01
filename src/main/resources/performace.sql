57c853a379020c410e66dea2b3f69b48972b5dc1
Luis Garcia <lgarcia@bluecacao.com>
2014-04-01 12:52:52 -0500

    Added performance.sql that contains the SQL script to enable other indexes on foreign keys that should improve performance.

---
+++ src/main/resources/performace.sql
@@ -0,0 +1,8 @@
+-- Table: roles
+-- Improves performace by preventing a full table scan.
+CREATE INDEX ON roles (system_user);
+
+-- Table: users
+-- Improves performace by preventing a full table scan.
+CREATE INDEX ON users (active);
+CREATE INDEX ON users (username);
