5bc495bb961ea3a052bf906c5c1bc8eee4a562d1
lgarcia <lgarcia@10069-DESKTOP11.vLIGHTEDGE.VDI>
2014-04-03 14:12:42 -0500

    Code clean up - layout improvements.

--- src/main/java/com/bitjester/apps/common/utils/FacesUtil.java
+++ src/main/java/com/bitjester/apps/common/utils/FacesUtil.java
@@ -7,21 +7,23 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 public abstract class FacesUtil {
+	// Misc. methods
+	public static void addMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(message));
+	}
+	
 	private static ExternalContext getEC() {
 		return FacesContext.getCurrentInstance().getExternalContext();
 	}
 
+	public static void invalidateSession() {
+		getEC().invalidateSession();
+	}
+	
+	// Navigation methods
 	private static String getCPath() {
 		// Returns String the represents the URI of the deployed app's ROOT.
 		return getEC().getRequestContextPath() + "/";
-	}
-	
-	public static void addMessage(String message) {
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(message));
-	}
-
-	public static void invalidateSession() {
-		getEC().invalidateSession();
 	}
 
 	public static void navTo(String url) {
