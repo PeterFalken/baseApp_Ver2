74135a7015de050a8d04faddde5587c1cc9d94bb
luis <luis@gallia.local>
2014-08-25 10:50:32 -0600

    Upadte

--- src/main/java/com/bitjester/apps/common/utils/CodeUtil.java
+++ src/main/java/com/bitjester/apps/common/utils/CodeUtil.java
@@ -4,11 +4,13 @@ import java.util.Random;
 
 public abstract class CodeUtil {
 	public static Random r;
-	
+
 	public static String generateCode(char z) {
 		if (null == r)
 			r = new Random();
 		String base = "xxxx-xxx" + z + "-" + z + "xxx-xxxx";
-		return base.replace("x", Integer.toHexString(r.nextInt(16))).toUpperCase();
+		while (base.contains("x"))
+			base = base.replaceFirst("[x]", Integer.toHexString(r.nextInt(16)));
+		return base.toUpperCase();
 	}
 }
