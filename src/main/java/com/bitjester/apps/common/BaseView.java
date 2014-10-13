aac6057161173628c6a34a798be1ebd4b851b4cc
lgarcia <lgarcia@10069-DESKTOP11.vLIGHTEDGE.VDI>
2014-10-13 01:03:49 -0500

    Added abstract class BaseView. It holds a BookKeeper, EntityManager & Logger references for general Views - It is based as a Named object. Scope depends on Developer.

---
+++ src/main/java/com/bitjester/apps/common/BaseView.java
@@ -0,0 +1,22 @@
+package com.bitjester.apps.common;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import com.bitjester.apps.common.utils.BookKeeper;
+
+@Named
+public abstract class BaseView implements Serializable {
+	protected static final long serialVersionUID = 1L;
+	@Inject
+	protected BookKeeper bk;
+	@Inject
+	protected EntityManager em;
+	@Inject
+	protected Logger log;
+	
+}
